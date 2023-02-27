aeb62a7c49d459c7511a9de54113a49cfc3474f8
ErturanDosanov <120151264+ErturanDosanov@users.noreply.github.com>
2023-02-27 16:21:21 +0300

    Delete home_work_3 directory

--- home_work_3/bin/home_work_3.dart
+++
@@ -1,6 +0,0 @@
-import 'package:home_work_3/home_work_3.dart' as home_work_3;
-
-void main(List<String> arguments) {
-  print('Hello world: ${home_work_3.calculate()}!');
-  print('4+6');
-}
--- home_work_3/test/home_work_3_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:home_work_3/home_work_3.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
